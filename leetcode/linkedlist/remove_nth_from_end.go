54e09342a088e8137b444319a61c25457bebbfa4
Pepper Yun Tianjiao <pepper.yuntj@shopee.com>
2025-04-06 21:34:12 +0800

    linkedlist - easy

--- leetcode/linkedlist/remove_nth_from_end.go
+++ leetcode/linkedlist/remove_nth_from_end.go
@@ -1,54 +1,6 @@
 // https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 package main
 
-import "fmt"
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-type MyLinkedList struct {
-	head *ListNode
-}
-
-func Constructor() MyLinkedList {
-	head := &ListNode{}
-	return MyLinkedList{
-		head: head,
-	}
-}
-
-func (l *MyLinkedList) AddAtTail(val int) {
-	tailNode := &ListNode{
-		Val: val,
-	}
-	node := l.head
-	for node.Next != nil {
-		node = node.Next
-	}
-	node.Next = tailNode
-}
-
-func (l *MyLinkedList) PrintLinkedList() {
-	node := l.head
-	for node.Next != nil {
-		node = node.Next
-		fmt.Println(node.Val)
-	}
-}
-
-func (l *MyLinkedList) Len() int {
-	length := 0
-	node := l.head
-	for node.Next != nil {
-		node = node.Next
-		length++
-	}
-
-	return length
-}
-
 func (l *MyLinkedList) removeNthFromEnd(n int) *ListNode {
 	node := l.head
 	for i := 0; node.Next != nil && i < l.Len()-n; i++ {
